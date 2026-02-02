24e8c8481a860a21b54654f31f4f888c57deb3ed
suyog khadka <ksuyog697@gmail.com>
2026-02-02 20:41:03 +0545

    lerned lambda in the python

---
+++ lambda_basic.py
@@ -0,0 +1,12 @@
+'''syntax'''
+# lambda arguments: expression
+
+'''greater of two number using  the lambda function  '''
+
+# greater=lambda num1,num2 : num1 if num1>num2 else num2
+# print(greater(100,200))
+
+'''concacate the string using the lambda function  '''
+
+# full_name= lambda str1,str2 : str1+" " +str2
+# print(full_name("suyog" ,"khadka"))
