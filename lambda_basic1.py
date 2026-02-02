7c8ed1a7ec30db778ec25fe875d77fd86744cc9e
suyog khadka <ksuyog697@gmail.com>
2026-02-02 20:47:53 +0545

    learned lambda in the python

---
+++ lambda_basic1.py
@@ -0,0 +1,16 @@
+# use lambda function if we want tio make the code in the one line 
+# use the lambda fun is we want to pass the function as an argument 
+
+
+#lambda function 
+# SYNTAX:
+#function = lambda  , variable : to do task(block of code )
+
+
+'''double = lambda x: x*2
+cube = lambda x: x*x*x
+avg = lambda x,y : (x+y)/2
+a=(avg(4,2))
+a=int(a)
+print(a)'''
+
