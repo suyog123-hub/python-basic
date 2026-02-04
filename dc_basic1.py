b70be68a50574a0c7f48b14153260491b085e5a1
suyog khadka <ksuyog697@gmail.com>
2026-02-04 23:01:50 +0545

    basic of the data conversion

---
+++ dc_basic1.py
@@ -0,0 +1,16 @@
+#string to boolean 
+    # print(bool("true"))
+    # print(bool("false"))
+    # print(bool("hello"))
+
+# integer to boolean 
+# except 0 everything is true while converting interger into py
+    # print(bool(100))
+    # print(bool(0))
+    # print(bool(100.100))
+
+#flaot to integer
+    # print(int(1222.33))
+
+
+# print(int(bool("True")))
