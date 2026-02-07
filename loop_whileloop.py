46d11977a2dc38cae82bc896a3cc9b0a525662ae
suyog khadka <ksuyog697@gmail.com>
2026-02-07 22:44:14 +0545

    learned about the forloop

---
+++ loop_whileloop.py
@@ -0,0 +1,65 @@
+ 
+# while loop :
+     # block of code 
+
+'''count=1
+while count<=5:
+    print("hello",count)
+    count +=1
+print(count)'''
+
+# printing in forwarnd moment
+i=1
+'''while i<=10:
+    print(i)
+    i=i+1'''
+
+# printing in reverse moment 
+'''i=10
+while i>=1:
+    print(i)
+    i=i-1'''
+# question 
+#1---> 
+  # print number from 1 to 100
+'''i=1
+while i<=100:
+    print(i)
+    i=i+1
+'''
+# print the number from 100 to 1
+
+'''i=100
+while i>=1:
+    print(i)
+    i=i-1'''
+
+# print the multiplication table of a number n
+'''num = int(input("Enter the number: "))
+i = 1
+while i <= 10:
+    print(num, "*", i, "=", num * i)
+    i = i + 1'''
+'''
+i=1
+while i<=10:
+    a=print(i*i)  # how to print in the list format 
+    i+=1'''
+
+'''num=(1,2,3,4,5,6,7,8)
+num1=int(input("enter the number you want to cehck"))
+i=0
+while i< len(num) :
+    if(num[i] == num1):
+        print("number found")
+        i=i+1'''
+
+
+'''a=[1,2,3,4,5]
+b=[1,2,3,6,7]
+
+for i in a:
+    for j in b:
+        if i==b:
+         print(i)'''
+
