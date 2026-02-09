58d97d9381fa5b8ccd2036319ef0a6c1040419f7
suyog khadka <ksuyog697@gmail.com>
2026-02-09 21:26:21 +0545

    learned about the recursion in the python

---
+++ resursion_basic.py
@@ -0,0 +1,61 @@
+# type conversion --> the process of converting one data type to another data type
+
+# type implicit (data conversion)
+# type explicit (type casting) python intrepreter automatically convert one data type to another
+
+# empty string is false in  the boolean 
+# while converting int to boolean except 0,everything is false,only is there is  0  then it is true
+#string to float 
+    # num=float("333.5")
+    # print(num,type(num))
+# string to boolean 
+print(bool)
+
+
+# type explictt (data converesion) # user convert the data type 
+# a=10
+# b=2
+# c=10/2
+# print(type(c))
+# c=int(c)
+# print(type(c))
+
+# a="sujan"
+# b=set(a)
+# print(b)
+# print(type(b))
+
+# a="sujan"
+# b=list(a)
+# print(b)
+# print(type(b))
+
+# a="12.12"
+# b=float(a)
+# c=int(b)
+# print(type(c))
+# print(c) 
+
+# a={
+#     "name":'sujan',
+#     "age":23
+# }
+# b=tuple(a.items())
+# print(b)
+
+# a=True # here true means 1 so the answer is 4
+# b=3
+# c= a+b
+# print(c)
+
+# a=[2,1,2,2,2,2,3,33,44,4,4,4,4,4.56]
+# a.sort(reverse=True)
+# print(a)
+# c=set(a)
+# b=list(c)
+# print(b)
+
+
+
+
+
